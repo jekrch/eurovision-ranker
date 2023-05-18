6a4709642f81a327cab17633fd81bac1d57808a1
Jacob Krch <jekrch@gmail.com>
2023-05-17 21:36:06 -0500

    switch to react-beautiful-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,64 @@
 
-import { render } from 'react-dom'
-import Ranking from './components/Ranking'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { TouchBackend } from 'react-dnd-touch-backend';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { useState } from 'react';
+import { Card } from './components/Card';
+import { countries2023 } from './data/Countries';
 
 function App() {
+
+  const [items, setItems] = useState(countries2023);
+
+  function handleOnDragEnd(result: any) {
+    console.log(result)
+    if (!result.destination) return;
+    const itemsArray = Array.from(items);
+    const [reorderedItem] = itemsArray.splice(result.source.index, 1);
+    itemsArray.splice(result.destination.index, 0, reorderedItem);
+    setItems(itemsArray);
+  }
+
   return (
-    <div className="bg-gray-800 min-h-screen">
-      <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-        <Ranking />
-      </DndProvider>
+    <div className="bg-[#040241] min-h-screen">
+
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="items">
+          {(provided) => (
+            <ul 
+              {...provided.droppableProps} 
+              ref={provided.innerRef}
+              className="m-auto pt-10"
+            >
+              {items.map(({ id, content }, index) => (
+                <Draggable key={id.toString()} draggableId={id.toString()} index={index}>
+                {(provided: any) => (
+                  <li 
+                    ref={provided.innerRef} 
+                    {...provided.draggableProps} 
+                    {...provided.dragHandleProps}
+                  >
+                    <Card 
+                      id={id} 
+                      className="w-60 m-auto text-slate-400 bg-black"
+                      name={content}
+                    />
+                  </li>
+                )}
+              </Draggable>
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+
     </div>
+
+
   )
 }
 
-const rootElement = document.getElementById('root')
-render(<App />, rootElement)
+// const rootElement = document.getElementById('root')
+// render(<App />, rootElement)
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,4 @@
-import type { Identifier, XYCoord } from 'dnd-core'
 import type { FC } from 'react'
-import { useRef } from 'react'
-import { useDrag, useDrop } from 'react-dnd'
-
-import ItemTypes from '../Constants'
 
 const style = {
   border: '1px dashed gray',
@@ -16,95 +11,22 @@ const style = {
 
 export interface CardProps {
   id: any
-  text: string
-  index: number
+  name: string
   className: string
-  moveCard: (dragIndex: number, hoverIndex: number) => void
 }
 
-interface DragItem {
-  index: number
-  id: string
-  type: string
-}
+export const Card: FC<CardProps> = (props) => {
 
-export const Card: FC<CardProps> = ({ id, text, index, className, moveCard }) => {
-  const ref = useRef<HTMLDivElement>(null)
-  const [{ handlerId }, drop] = useDrop<
-    DragItem,
-    void,
-    { handlerId: Identifier | null }
-  >({
-    accept: ItemTypes.COUNTRY,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      }
-    },
-    hover(item: DragItem, monitor) {
-      if (!ref.current) {
-        return
-      }
-      const dragIndex = item.index
-      const hoverIndex = index
-
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return
-      }
-
-      // Determine rectangle on screen
-      const hoverBoundingRect = ref.current?.getBoundingClientRect()
-
-      // Get vertical middle
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
-
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset()
-
-      // Get pixels to the top
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
-
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return
-      }
-
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return
-      }
-      moveCard(dragIndex, hoverIndex)
-
-      item.index = hoverIndex
-    },
-  })
-
-  const [{ isDragging }, drag] = useDrag({
-    type: ItemTypes.COUNTRY,
-    item: () => {
-      return { id, index }
-    },
-    collect: (monitor: any) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  })
-
-  const opacity = isDragging ? 0 : 1
-  drag(drop(ref))
+  //const ref = useRef<HTMLDivElement>(null);
   return (
     <div 
-      ref={ref} 
-      className={className} 
-      style={{ ...style, opacity }} 
-      data-handler-id={handlerId}
+      key={props.id}
+      //ref={ref} 
+      className={props.className} 
+      style={{ ...style }} 
     >
-      {text}
+      {props.name}
     </div>
   )
 }
+

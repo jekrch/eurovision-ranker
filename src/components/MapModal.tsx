45c45f447973931f18efdc89b847c61cf786bea9
Jacob Krch <jekrch@gmail.com>
2024-01-01 15:52:20 -0600

    file cleanup and modal tweaks

--- src/components/MapModal.tsx
+++ src/components/MapModal.tsx
@@ -52,6 +52,7 @@ const MapModal: React.FC<MapModalProps> = ({ isOpen, onClose }) => {
     return (
         <Modal 
             className="fixed inset-0 z-10 overflow-y-auto pb-2 pt-5 px-2 min-w-[80vw] max-h-[80vh]" 
+            closeBtnClassName='mt-[0.6em] mr-[0.5em]'
             isOpen={isOpen} onClose={onClose}
         >
             <div className="mb-1 -mt-2 mb-2 w-full font-strong text-center rounded-md ">Ranking heat map</div>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,6 +4,7 @@ import React, { ReactNode, useEffect, useRef, useState } from 'react';
 type ModalContainerProps = {
     isOpen: boolean;
     className: string;
+    closeBtnClassName?: string;
     onClose: () => void;
     children: ReactNode;
 };
@@ -71,9 +72,19 @@ const Modal: React.FC<ModalContainerProps> = (props: ModalContainerProps) => {
                         props.className
                     )}
             >
-                <button onClick={props.onClose}
-                    className="absolute top-0 right-0 mt-4 mr-4 text-gray-300 text-lg leading-none hover:text-gray-400">
-                    &#x2715;
+                <button 
+                    type="button" 
+                    className={
+                        classNames(
+                            "absolute top-0 right-0 mt-4 mr-4 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white", 
+                            props.closeBtnClassName)}
+                    data-modal-hide="default-modal"
+                    onClick={props.onClose}
+                >
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
                 </button>
                 {props.children}
             </div>

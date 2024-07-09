f7ef8b48714c6e6fef3a67bcd3962882c4e316d9
jekrch <jekrch@gmail.com>
2024-07-09 18:52:04 -0500

    tooltip tweak

--- src/components/modals/MainModal.tsx
+++ src/components/modals/MainModal.tsx
@@ -88,7 +88,7 @@ const MainModal: React.FC<MainModalProps> = (props: MainModalProps) => {
                         target="_blank"
                         rel="noopener noreferrer"
                         href="https://github.com/jekrch/eurovision-ranker/releases"
-                    >v4.0.1</a>
+                    >v4.0.2</a>
                 </span>
                 <span className="text-right">
                     {`Copyright (c) 2023${new Date().getFullYear()?.toString() !== '2023' ? '-' + new Date().getFullYear() : ''} `}

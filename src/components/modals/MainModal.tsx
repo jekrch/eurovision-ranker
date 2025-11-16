5605cd19ba804b186a030f3b9d61e760edd29969
jekrch <jekrch@gmail.com>
2025-11-16 16:03:24 -0600

    version

--- src/components/modals/MainModal.tsx
+++ src/components/modals/MainModal.tsx
@@ -88,7 +88,7 @@ const MainModal: React.FC<MainModalProps> = (props: MainModalProps) => {
                         target="_blank"
                         rel="noopener noreferrer"
                         href="https://github.com/jekrch/eurovision-ranker/releases"
-                    >v9.0</a>
+                    >v10.0</a>
                 </span>
                 <span className="text-right">
                     {`Copyright (c) 2023${new Date().getFullYear()?.toString() !== '2023' ? '-' + new Date().getFullYear() : ''} `}
